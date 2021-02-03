a02c6435156929e44d7cb92f1ec6668fc98cd6d9
ericyeung <shiwei.yang@matrixport.com>
2021-02-04 00:57:20 +0800

    Update http utils

--- http_utils.go
+++ http_utils.go
@@ -7,16 +7,22 @@ import (
 	"time"
 )
 
+type HttpRespData struct {
+	StatusCode  int
+	RespHeader  *http.Header
+	RespContent []byte
+}
+
 func NewHttpClientWithTimeout(dur time.Duration) *http.Client {
 	return &http.Client{
 		Timeout: dur,
 	}
 }
 
-func DoHttp(client *http.Client, method string, url string, headers map[string]string, body io.Reader) (int, []byte, error) {
+func DoHttp(client *http.Client, method string, url string, headers map[string]string, body io.Reader) (*HttpRespData, error) {
 	request, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
 	for k, v := range headers {
@@ -26,19 +32,23 @@ func DoHttp(client *http.Client, method string, url string, headers map[string]s
 	resp, err := client.Do(request)
 
 	if err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
 	defer func() { _ = resp.Body.Close() }()
 
 	buf, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
-	return resp.StatusCode, buf, nil
+	return &HttpRespData{
+		StatusCode:  resp.StatusCode,
+		RespHeader:  &resp.Header,
+		RespContent: buf,
+	}, nil
 }
 
-func DoHttpGet(client *http.Client, url string, headers map[string]string) (int, []byte, error) {
+func DoHttpGet(client *http.Client, url string, headers map[string]string) (*HttpRespData, error) {
 	return DoHttp(client, http.MethodGet, url, headers, nil)
 }
