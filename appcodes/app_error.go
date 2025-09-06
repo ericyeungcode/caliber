b11cdceecc6d4fb2f50055ab5146a4155190f44e
ericyeung <enjoyericyeung@gmail.com>
2025-09-06 13:04:22 +0800

    Update AppError

--- appcodes/app_error.go
+++ appcodes/app_error.go
@@ -8,34 +8,32 @@ type AppError struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 
-	// make vars private, so that it can't be set via global AppError variables directly
-	// global AppError variables(InvalidArgumentError ...) are shared across the system, so we don't want to allow setting vars directly
-	vars map[string]any `json:"vars,omitempty"`
+	// make details private, so that it can't be set via global AppError variables directly
+	// global AppError variables(InvalidArgumentError ...) are shared across the system, so we don't want to allow setting details directly
+	details map[string]any `json:"details,omitempty"`
 }
 
 func NewAppError(code int, message string) *AppError {
 	return &AppError{
 		Code:    code,
 		Message: message,
-		vars:    nil,
+		details: nil,
 	}
 }
 
-func NewAppErrorVars(code int, message string, ctxVars map[string]any) *AppError {
+func NewAppErrorVars(code int, message string, details map[string]any) *AppError {
 	return &AppError{
 		Code:    code,
 		Message: message,
-		vars:    ctxVars,
+		details: details,
 	}
 }
 
 // Exported method for interacting with appError
 func (e *AppError) Error() string {
-	return fmt.Sprintf("[%v;%v;%+v]", e.Code, e.Message, e.vars)
+	return fmt.Sprintf("[%v;%v;%+v]", e.Code, e.Message, e.details)
 }
 
-func (e *AppError) WithCtxVars(ctxVars map[string]any) *AppError {
-	v := *e // Create a copy of the current error
-	v.vars = ctxVars
-	return &v
+func (e *AppError) ClonedWithDetails(details map[string]any) *AppError {
+	return NewAppErrorVars(e.Code, e.Message, details)
 }
